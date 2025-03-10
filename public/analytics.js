c2fed854006374b9966f28767b57621abc1b23aa
HamadaSalhab <hamada.a.salhab@gmail.com>
2025-03-10 18:10:31 +0300

    Minor fix in Google Analytics

--- public/analytics.js
+++ public/analytics.js
@@ -1,12 +1,15 @@
-// Google tag (gtag.js)
-const script = document.createElement("script");
-script.src = "https://www.googletagmanager.com/gtag/js?id=G-PRBSQ355GE";
-script.async = true;
-document.head.appendChild(script);
+// Wait for the page to load
+window.addEventListener("load", function () {
+  // Google tag (gtag.js)
+  const script = document.createElement("script");
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-PRBSQ355GE";
+  script.async = true;
+  document.head.appendChild(script);
 
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
-gtag("config", "G-PRBSQ355GE");
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+  gtag("config", "G-PRBSQ355GE");
+});
