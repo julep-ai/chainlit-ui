90488f565f31a35369fe049f9d52d928c6d5ca38
HamadaSalhab <hamada.a.salhab@gmail.com>
2025-03-10 18:00:43 +0300

    Add google analytics

---
+++ public/analytics.js
@@ -0,0 +1,12 @@
+// Google tag (gtag.js)
+const script = document.createElement("script");
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-PRBSQ355GE";
+script.async = true;
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-PRBSQ355GE");
