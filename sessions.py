e26a4ec019f3c6f5b71fd490adc47dc9e098a8b0
HamadaSalhab <hamada.a.salhab@gmail.com>
2025-03-10 17:50:46 +0300

    Refactor

--- sessions.py
+++ sessions.py
@@ -8,36 +8,36 @@ agent_id = os.getenv("AGENT_UUID")
 system_template1 = open("system-templates/system-template-1.jinja").read()
 system_template2 = open("system-templates/system-template-2.jinja").read()
 system_template3 = open("system-templates/system-template-3.jinja").read()
-session1 = {
+session_hybrid_mmr = {
     "agent": agent_id,
     "system_template": None,
     "recall_options": {
         "mode": "hybrid",
         "num_search_messages": 4,
         "max_query_length": 1000,
-        "confidence": 0.5,
+        "confidence": 0.7,
         "alpha": 0.5,
         "limit": 10,
-        "mmr_strength": 0.5,
+        "mmr_strength": 0.7,
     },
 }
 
-session2 = {
+session_vector = {
     "agent": agent_id,
     "system_template": None,
     "recall_options": {
         "mode": "vector",
         "num_search_messages": 4,
         "max_query_length": 1000,
-        "confidence": 0.5,
+        "confidence": 0.7,
         "limit": 10,
     },
 }
 
 
 sessions = {
-    "Main Agent": session1,
-    "Plain Vector Agent": session2,
+    "Tia (Hybrid + MMR)": session_hybrid_mmr,
+    "Tia (Vector)": session_vector,
 }
 
 system_templates = {
